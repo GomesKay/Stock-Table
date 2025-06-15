bbeadfa96d5794cc797bbed968bdd17b42faadbe
caiogomes <caio2011gs@gmail.com>
2025-06-15 19:06:57 -0300

    Setup Inicial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center text-white">
+      <h1 className="font-heading text-5xl font-semibold">
+        Sistema de Estoque
+      </h1>
+    </div>
+  )
+}
